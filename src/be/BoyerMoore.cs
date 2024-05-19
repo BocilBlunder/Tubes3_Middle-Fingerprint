e68c9c0585ed4f3df44cb372989ce9e47d4456f1
RealAzzmi <azmibazeid@gmail.com>
2024-05-19 17:34:20 +0700

    feat: implement BoyerMoore & 3 distance functions

---
+++ src/be/BoyerMoore.cs
@@ -0,0 +1,73 @@
+
+public class BooyerMoore
+{
+
+    public static int[,] BadCharacterTable(string pattern)
+    {
+        int[,] table = new int[26, pattern.Length];
+        int[] alphabet = new int[26];
+        for (int i = 0; i < alphabet.Length; i++) alphabet[i] = -1;
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            int c = pattern[i] - 'a';
+            alphabet[c] = i;
+            for (int j = 0; j < 26; j++) table[j, i] = alphabet[j];
+        }
+        return table;
+    }
+
+
+    // Bisa dalam linier waktu, tapi kuadratik dulu aja. Kalau perlu, nanti dibuat yang linier.
+    public static int[] GoodFoundSuffixTable(string pattern)
+    {
+        int[] longestMatchedSuffix = new int[pattern.Length];
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            int j = i;
+            while (j >= 0 && pattern[pattern.Length - 1 - (i - j)] == pattern[j]) j--;
+            longestMatchedSuffix[i] = i - j;
+        }
+        int[] suffixTable = new int[pattern.Length];
+        for (int i = pattern.Length - 1; i >= 0; i--)
+        {
+            int j = pattern.Length - 2, suffixLength = pattern.Length - i;
+            while (j >= 0 && longestMatchedSuffix[j] < suffixLength) j--;
+            if (j < 0) suffixTable[i] = -1;
+            else suffixTable[i] = j;
+        }
+        return suffixTable;
+    }
+
+    public static bool Search(string pattern, string text)
+    {
+        if (text.Length < pattern.Length || text.Length == 0 || pattern.Length == 0)
+        {
+            return false;
+        }
+
+        int[,] badCharacterTable = BadCharacterTable(pattern);
+        int[] goodFoundSuffixTable = GoodFoundSuffixTable(pattern);
+
+        int currentPattern = 0;
+        while (currentPattern + pattern.Length - 1 < text.Length)
+        {
+            int currentIndex = currentPattern + pattern.Length - 1;
+            while (currentIndex >= currentPattern && pattern[currentIndex - currentPattern] == text[currentIndex]) currentIndex--;
+            if (currentIndex < currentPattern) return true;
+            int badShift;
+            if (badCharacterTable[text[currentIndex] - 'a', currentIndex - currentPattern] == -1) badShift = pattern.Length;
+            else badShift = (currentIndex - currentPattern) - badCharacterTable[text[currentIndex] - 'a', currentIndex - currentPattern];
+
+            int goodShift;
+            if (currentPattern + pattern.Length - 1 == currentIndex) goodShift = badShift;
+            else if (goodFoundSuffixTable[(currentIndex - currentPattern) + 1] == -1) goodShift = 1;
+            else goodShift = pattern.Length - goodFoundSuffixTable[(currentIndex - currentPattern) + 1] - 1;
+
+            int maxShift = Math.Max(badShift, goodShift);
+            currentPattern += maxShift;
+        }
+        return false;
+    }
+
+}
---
+++ src/be/StringDistance.cs
@@ -0,0 +1,71 @@
+using System;
+
+public class StringDistance
+{
+    public static int HammingDistance(string a, string b)
+    {
+        if (a.Length != b.Length)
+        {
+            Console.WriteLine("Strings must be of equal length in calculating the hamming distance");
+            Environment.Exit(0);
+        }
+        int count = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int LongestCommonSubsequence(string a, string b)
+    {
+        int[,] dp = new int[a.Length + 1, b.Length + 1];
+        for (int i = 1; i <= a.Length; ++i)
+        {
+            for (int j = 1; j <= b.Length; ++j)
+            {
+                if (a[i - 1] == b[j - 1])
+                {
+                    dp[i, j] = 1 + dp[i - 1, j - 1];
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+        }
+        return dp[a.Length, b.Length];
+    }
+
+    public static int LevenshteinDistance(string a, string b)
+    {
+        int[,] dp = new int[a.Length + 1, b.Length + 1];
+
+        for (int i = 1; i <= a.Length; i++)
+        {
+            dp[i, 0] = i;
+        }
+
+        for (int j = 1; j <= b.Length; j++)
+        {
+            dp[0, j] = j;
+        }
+
+        for (int i = 1; i <= a.Length; i++)
+        {
+            for (int j = 1; j <= b.Length; j++)
+            {
+                int cost;
+                if (a[i - 1] == b[j - 1]) cost = 0;
+                else cost = 1;
+
+                dp[i, j] = Math.Min(Math.Min(dp[i - 1, j] + 1, dp[i, j - 1] + 1), dp[i - 1, j - 1] + cost);
+            }
+        }
+        return dp[a.Length, b.Length];
+    }
+}
+
